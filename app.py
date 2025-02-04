25573aded8cb35a7e9dd773eb63a1147b081c25b
Serbanica123 <serbanalexandrunicolae@gmail.con>
2025-02-04 23:09:55 +0100

    Update

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ class TurretApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Turret Control")
-        
+        self.geometry('2560x1440')
         # Load main UI window
         self.main_window = TurretWindow(self)
+       
         self.main_window.pack()
 
     def run(self):
--- gui/main_window.py
+++ gui/main_window.py
@@ -1,22 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 import cv2
 from controllers.events import on_canvas_click
 
 class TurretWindow(tk.Frame):
     def __init__(self, parent):
-        super().__init__(parent)
-
+        super().__init__(parent, width=2560, height=1440, bd=1, relief='solid')
+        self.pack_propagate(False)
         # Load and convert OpenCV image
-        self.img = cv2.imread("1.png", 1)
+        self.img = cv2.imread("assets/3.jpg", 1)
         self.rgb_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
         self.pil_img = Image.fromarray(self.rgb_img)
         self.img_tk = ImageTk.PhotoImage(self.pil_img)
 
         # Create a canvas
-        self.canvas = tk.Canvas(self, width=self.img_tk.width(), height=self.img_tk.height())
-        self.canvas.pack()
-
+        self.canvas = tk.Canvas(self, width=self.img_tk.width(), height=self.img_tk.height(), bd=2, relief="solid")
+        self.canvas.pack(side="left")
+        
         # Display the image on the canvas
         self.canvas_image = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img_tk)
 
